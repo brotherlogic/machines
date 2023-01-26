e2e0482dd5e07b426066e05dc1483fb7bae95095
Simon Tucker <brotherlogic@gmail.com>
2023-01-26 14:46:38 -0800

    Merge quietly

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 cd /home/simon/machines
 git fetch -pq
-git merge origin/main
+git merge -q origin/main
 if diff chead .git/refs/heads/main; then
 else
   if ansible-playbook --vault-password-file=.vault_pass install.yml; then
