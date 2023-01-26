cdd7ede8976375fafe35325ba75ae1da6411c292
Simon Tucker <brotherlogic@gmail.com>
2023-01-26 13:19:20 -0800

    Adds update process

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+cd /home/simon/machines
+git fetch -p
+git merge origin/main
+if diff chead .git/refs/heads/main; then
+else
+  if ansible-playbook --vault-password-file=.vault_pass install.yml; then
+    cp .git/refs/heads/main chead
+  fi
+fi
